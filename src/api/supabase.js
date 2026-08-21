b70c96328428719f225927e108d8898a7536648d
jrodriguezs7-art <jrodriguezs7@uteq.edu.ec>
2026-08-21 00:13:32 -0500

    six commit

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const getSensorData = async () => {
   try {
